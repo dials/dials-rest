fefc74cf8fd08d8d1a0bdc5e844f1c32ac0a1aec
Richard Gildea <richard.gildea@diamond.ac.uk>
2023-04-05 13:54:03 +0100

    Optionally read secrets from directory

--- src/dials_rest/settings.py
+++ src/dials_rest/settings.py
@@ -1,5 +1,17 @@
+from __future__ import annotations
+
+from functools import lru_cache
+
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     jwt_secret: str
+
+    @staticmethod
+    @lru_cache
+    def get():
+        return Settings()
+
+    class Config:
+        secrets_dir = "/opt/secrets"
