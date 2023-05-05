2f0157f7648788a926815da8e414516e6e573733
Richard Gildea <rjgildea@users.noreply.github.com>
2023-05-05 15:20:32 +0100

    Basic logging for export_bitmaps endpoint (#4)
    Remove unused Token class

--- src/dials_rest/main.py
+++ src/dials_rest/main.py
@@ -1,11 +1,14 @@
+import logging
+
 from fastapi import FastAPI
 
 from .routers import find_spots, image
 from .settings import Settings
 
+logging.basicConfig(level=logging.INFO)
+
 settings = Settings()
 app = FastAPI()
-
 
 app.include_router(find_spots.router)
 app.include_router(image.router)
@@ -13,4 +16,4 @@ app.include_router(image.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Bigger Applications!"}
+    return {"message": "Welcome to the DIALS REST API!"}
