569efbbfdce07a6a29bdd147e72691a2c92ae454
Richard Gildea <richard.gildea@diamond.ac.uk>
2023-05-23 17:00:03 +0100

    Add title/description/version to app
    Add top-level dials_rest.__version__ attribute

--- src/dials_rest/main.py
+++ src/dials_rest/main.py
@@ -1,15 +1,50 @@
+"""
+A RESTful API to a (limited) subset of DIALS functionality.
+
+Authorization is currently handled through simple [JSON Web
+Tokens](https://jwt.io/).
+
+## Usage Example: Python/Requests
+
+Given an authentication token you can use this API from python as:
+
+```python
+import pathlib
+import requests
+
+AUTHENTICATION_TOKEN = pathlib.Path("/path/to/token").read_text().strip()
+auth_header = {"Authorization": f"Bearer {AUTHENTICATION_TOKEN}"}
+base_url = "https://example-dials-rest.com"
+
+# e.g. creating data collection
+response = requests.post(
+    f"{base_url}/export_bitmaps",
+    headers=auth_header,
+    json={
+        # ... body schema, see API below ...
+    },
+)
+response.raise_for_status()  # Check if the call was successful
+```
+"""
+
 import logging
 
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
+from . import __version__
 from .routers import find_spots, image
 from .settings import Settings
 
 logging.basicConfig(level=logging.INFO)
 
 settings = Settings()
-app = FastAPI()
+app = FastAPI(
+    title="DIALS REST API",
+    description=__doc__,
+    version=__version__,
+)
 
 app.include_router(find_spots.router)
 app.include_router(image.router)
