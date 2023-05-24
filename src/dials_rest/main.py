2bb07b78571bcb3956ed1876f881501d8a4f3903
Richard Gildea <rjgildea@users.noreply.github.com>
2023-05-24 11:02:34 +0100

    Improved OpenAPI docs (#8)
    * Redirect / to /docs
    * Add title/description/version to app
    * Add top-level dials_rest.__version__ attribute
    * Add examples to the export_bitmaps endpoint
    * Add examples to the find_spots endpoint
    * Add tags_metadata to app

--- src/dials_rest/main.py
+++ src/dials_rest/main.py
@@ -1,14 +1,64 @@
+"""
+A RESTful API to a (limited) subset of DIALS functionality.
+
+Authorization is currently handled through simple [JSON Web
+Tokens](https://jwt.io/).
+
+## Usage Example: Python/Requests
+
+Given an authentication token you can use this API from python as:
+
+```python
+import pathlib
+import requests
+
+AUTHENTICATION_TOKEN = pathlib.Path("/path/to/token").read_text().strip()
+auth_header = {"Authorization": f"Bearer {AUTHENTICATION_TOKEN}"}
+base_url = "https://example-dials-rest.com"
+
+# e.g. creating data collection
+response = requests.post(
+    f"{base_url}/export_bitmaps",
+    headers=auth_header,
+    json={
+        # ... body schema, see API below ...
+    },
+)
+response.raise_for_status()  # Check if the call was successful
+```
+"""
+
 import logging
 
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
+from . import __version__
 from .routers import find_spots, image
 from .settings import Settings
 
 logging.basicConfig(level=logging.INFO)
 
+
+tags_metadata = [
+    {
+        "name": "images",
+        "description": "Generate bitmaps of diffraction images",
+    },
+    {
+        "name": "spotfinding",
+        "description": "Run spotfinding on a diffraction image and report summary statistics",
+    },
+]
+
+
 settings = Settings()
-app = FastAPI()
+app = FastAPI(
+    title="DIALS REST API",
+    description=__doc__,
+    openapi_tags=tags_metadata,
+    version=__version__,
+)
 
 app.include_router(find_spots.router)
 app.include_router(image.router)
@@ -23,6 +73,6 @@ if settings.enable_metrics:
     instrumentator.expose(app)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Welcome to the DIALS REST API!"}
+@app.get("/", include_in_schema=False)
+def get_root():
+    return RedirectResponse("/docs")
