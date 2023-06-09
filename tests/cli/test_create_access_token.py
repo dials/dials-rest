2891b518d415749ec7e9f45bcebeb24ad94d25b3
Richard Gildea <richard.gildea@diamond.ac.uk>
2023-06-09 10:45:44 +0100

    Import inline because of monkeypatching of env var
    by access_token fixture

--- tests/cli/test_create_access_token.py
+++ tests/cli/test_create_access_token.py
@@ -4,11 +4,11 @@ from datetime import datetime
 import jose.jwt
 from dateutil.tz import UTC
 
-from dials_rest import auth
-from dials_rest.cli import create_access_token
 
+def test_create_access_token_default_expiry_time(access_token, capsys):
+    from dials_rest import auth
+    from dials_rest.cli import create_access_token
 
-def test_create_access_token_default_expiry_time(capsys):
     create_access_token.run(args=[])
     captured = capsys.readouterr()
     token = captured.out.strip()
@@ -18,7 +18,10 @@ def test_create_access_token_default_expiry_time(capsys):
     assert exp - now <= auth.ACCESS_TOKEN_EXPIRE_MINUTES * 60
 
 
-def test_create_access_token_user_expiry_time(capsys):
+def test_create_access_token_user_expiry_time(access_token, capsys):
+    from dials_rest import auth
+    from dials_rest.cli import create_access_token
+
     now = timegm(datetime.utcnow().utctimetuple())
     delta = 30
     expiry = datetime.fromtimestamp(now + delta, tz=UTC)
